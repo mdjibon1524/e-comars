f8f69504f9ec6fb914c91f15c121b24935886e8e
mdjibon1524 <skjibon15740@gmail.com>
2025-08-06 16:58:38 +0600

    Create products.js

---
+++ products.js
@@ -0,0 +1,30 @@
+// products.js
+const products = [
+  {
+    id: 1,
+    name: "Premium Polo Shirt",
+    price: 500,
+    oldPrice: 600,
+    sizes: ["S", "M", "L", "XL"],
+    category: "পুরুষদের পোশাক",
+    img: "https://i.imgur.com/G8CUkng.jpeg"
+  },
+  {
+    id: 2,
+    name: "Ladies Premium Cotton Half Sleeve T-Shirt – DTF Print Edition",
+    price: 350,
+    oldPrice: 500,
+    sizes: ["M", "L"],
+    category: "মহিলাদের পোশাক",
+    img: "https://i.imgur.com/enndo4a.jpeg"
+  },
+  {
+    id: 3,
+    name: "Mash T-Shirt and Short Pant Set",
+    price: 400,
+    oldPrice: 550,
+    sizes: ["S", "M", "L", "XL"],
+    category: "অফার",
+    img: "https://i.imgur.com/G8CUkng.jpeg"
+  }
+];
